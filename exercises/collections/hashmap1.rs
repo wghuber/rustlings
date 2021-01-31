720c6f03f2980e3c8663269e6aa363c2973d6510
Wilhelm Huber <w.g.huber@gmail.com>
2021-02-01 00:39:18 +0100

    STL types

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,18 +11,18 @@
 // Execute the command `rustlings hint collections3` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new(); // TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
+    basket.insert(String::from("apple"), 4);
+    basket.insert(String::from("strawberry"), 10);
+    basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
-
     basket
 }
 
@@ -39,8 +39,6 @@ mod tests {
     #[test]
     fn at_least_five_fruits() {
         let basket = fruit_basket();
-        assert!(basket
-            .values()
-            .sum::<u32>() >= 5);
+        assert!(basket.values().sum::<u32>() >= 5);
     }
 }
--- exercises/option/option1.rs
+++ exercises/option/option1.rs
@@ -8,16 +8,30 @@ fn print_number(maybe_number: Option<u16>) {
     println!("printing: {}", maybe_number.unwrap());
 }
 
-fn main() {
-    print_number(13);
-    print_number(99);
-
-    let mut numbers: [Option<u16>; 5];
+fn print_numbers(maybe_numbers: &[Option<u16>; 5]) {
     for iter in 0..5 {
-        let number_to_add: u16 = {
-            ((iter * 1235) + 2) / (4 * 16)
-        };
+        let maybe_number = maybe_numbers[iter as usize];
 
-        numbers[iter as usize] = number_to_add;
+        if let Some(number) = maybe_number {
+            println!("printing: {}", number);
+        }
+        if maybe_number.is_some() {
+            println!("printing: {}", maybe_number.unwrap());
+        }
     }
+}
+
+fn main() {
+    print_number(Some(13));
+    print_number(Some(99));
+
+    let mut numbers: [Option<u16>; 5] = Default::default();
+    for iter in 0..5 {
+        let number_to_add: u16 = { ((iter * 1235) + 2) / (4 * 16) };
+
+        if let None = numbers[iter as usize] {
+            numbers[iter as usize] = Some(number_to_add);
+        }
+    }
+    print_numbers(&numbers);
 }
